0505e587223ac93bbc1b1ae10421dad2b506794d
Negin <negin.khademi@gmail.com>
2022-02-06 18:12:00 +0330

    merge master to develope_JS

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     // first page display block, others display none
     // click search box and go to the second page
@@ -6,27 +6,28 @@ $(document).ready(function() {
     // $("#first-div").css("display", "none");
     $("#second-div").css("display", "none");
     $("#third-div").css("display", "none");
-    $("#myBtn").click(function() {
-            $("#first-div").fadeOut(function() {
-                $("#second-div").fadeIn();
-                var searchResult = document.getElementById("searchResult");
-                searchResult.innerHTML = document.getElementById("tags").value;
-            });
-        })
-        // Get information from api and use autocomplete function for input
+    $("#myBtn").click(function () {
+        $("#first-div").fadeOut(function () {
+            $("#second-div").fadeIn();
+            var searchResult = document.getElementById("searchResult");
+            searchResult.innerHTML = document.getElementById("tags").value;
+        });
+    })
+
+    // Get information from api and use autocomplete function for input
     var availableTags = $.ajax({
         url: "https://dog.ceo/api/breeds/list/all",
         type: "GET",
         dataType: "json",
-        success: function(dogName) {
+        success: function (dogName) {
             let newArr = [];
-            Object.keys(dogName.message).map(function(key, index) {
+            Object.keys(dogName.message).map(function (key, index) {
                 newArr.push(key)
-                    // newArr = [
-                    //     ...newArr
-                    //     // ,...dogName.message[key]
-                    // ]
-                    // newArr2 = [...newArr,...dogName.message[key]]
+                // newArr = [
+                //     ...newArr
+                //     // ,...dogName.message[key]
+                // ]
+                // newArr2 = [...newArr,...dogName.message[key]]
             });
             console.log(newArr);
             console.log(newArr.length);
@@ -40,20 +41,20 @@ $(document).ready(function() {
     });
 
     // click previous key to return to the first page
-    $("#myBtn2").click(function() {
-            $("#second-div").fadeOut(function() {
-                $("#first-div").fadeIn();
-            });
-        })
-        // click one of the buttons and go to the third page
-    $(".myBtn3").click(function() {
-            $("#second-div").fadeOut(function() {
-                $("#third-div").fadeIn();
-            });
-        })
-        // click previous key to return to the second page
-    $("#myBtn4").click(function() {
-        $("#third-div").fadeOut(function() {
+    $("#myBtn2").click(function () {
+        $("#second-div").fadeOut(function () {
+            $("#first-div").fadeIn();
+        });
+    })
+    // click one of the buttons and go to the third page
+    $(".myBtn3").click(function () {
+        $("#second-div").fadeOut(function () {
+            $("#third-div").fadeIn();
+        });
+    })
+    // click previous key to return to the second page
+    $("#myBtn4").click(function () {
+        $("#third-div").fadeOut(function () {
             $("#second-div").fadeIn();
         });
     })
