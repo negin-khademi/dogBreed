fce5b3d64ddb4da3b287248993400981dc19f92f
Negin <negin.khademi@gmail.com>
2022-02-09 11:34:29 +0330

    preven sending empty form input

--- js/script.js
+++ js/script.js
@@ -1,18 +1,48 @@
 $(document).ready(function () {
+    // If a form field (fname) is empty, this function alerts a message, and returns false,
+    //  to prevent the form from being submitted.
+    function validateForm(){
+        let x = document.forms["myForm"]["fname"].value;
+        if (x == "") {
+            alert("Name must be filled out");
+            return false;
+        }
+    }
+    //    variables
+    let selector = {
+        id : {
+            firstDiv :'first-div',
+            secondDiv : 'second-div',
+            thirdDiv : 'thirdDiv',
+        },
+        class :{
+            first : 'first-page',
+            second : 'second-page',
+            third : 'third-page'
+        },
+
+    };
 
     // first page display block, others display none
     // click search box and go to the second page
     // Get data from input and send it to the next page
-    // $("#first-div").css("display", "none");
+
     $("#second-div").css("display", "none");
     $("#third-div").css("display", "none");
     $("#myBtn").click(function () {
-        $("#first-div").fadeOut(function () {
+        let x = document.forms["myForm"]["fname"].value;
+        if (x == "") {
+            validateForm();
+        }else{
+            $("#first-div").fadeOut(function () {
             $("#second-div").fadeIn();
             var searchResult = document.getElementById("searchResult");
             searchResult.innerHTML = document.getElementById("tags").value;
         });
+        }
+        
     })
+    // create keys and pass information to them
 
     // Get information from api and use autocomplete function for input
     var availableTags = $.ajax({
@@ -29,16 +59,48 @@ $(document).ready(function () {
                 // ]
                 // newArr2 = [...newArr,...dogName.message[key]]
             });
-            console.log(newArr);
+
+            // console.log(newArr);
+
             console.log(newArr.length);
-            // console.log(newArr2);
             $("#tags").autocomplete({
                 source: newArr
+            });
+
+            // press button and see the list of dog breed
+            $("#myBtn").click(function(e){
+                getDogBreed(this.innerText)
             });
 
         }
 
     });
+
+    // create getDogBreed function
+    // performance of this function is give us a list of dogBreed 
+    // related to dogName we chose before
+     
+    var getDogBreed = function(dogType){
+        console.log(dogType);
+        $.ajax({
+            url: "https://dog.ceo/api/breed/" + dogType + "/list",success:function (result){
+                const createKeys = document.getElementById("createKeys");
+                createKeys.innerHTML = "";
+                for(let i =0 ;i < dogType.length ;i++){
+                    const rowCls = ["row" ,"row-cols-2" ,"row-cols-md-4" ,"row-cols-lg-6" ,"gy-3" ,"justify-content-center" , "mx-auto" ,"d-flex" ,"text-center" ,"gx-3"];
+                    createKeys.classList.add(...rowCls);
+                    let column = document.createElement("div");
+                    document.getElementById("createKeys").appendChild(column);
+                    column.classList.add("col","col-btn");
+                    let myButton = document.createElement("button");
+                    const colCls = ["glow-on-hover" ,"p-5" ,"d-flex" ,"align-items-center" ,"myBtn3" ];
+                    myButton.classList.add(...colCls);
+                    
+
+                 }//end for loop
+            } // end function(result)
+         }) //end ajax 
+    }//end dogtype function  
 
     // click previous key to return to the first page
     $("#myBtn2").click(function () {
